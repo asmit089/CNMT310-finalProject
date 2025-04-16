da732e891e0fc8464f4221bf9e044e0a3a075067
Eric <ewolf725@uwsp.edu>
2025-04-16 11:07:59 -0500

    Fixed and finished the parsing of Username and Password from login.php. Will now check both of those through the WebService, still need to complete headers. Also checking to see if creds.php gets overwritten again.

--- final-project/authenticate-action.php
+++ final-project/authenticate-action.php
@@ -9,7 +9,6 @@ authenticates POST information sent from login.php
 
 session_start();
 //comment
-//check isset/empty for username & password $_POST
 //hii
 
 require_once("class/WebServiceClient.php");
@@ -18,9 +17,22 @@ require_once("../../creds.php");
 $url = "https://cnmt310.classconvo.com/bookmarks/";
 $client = new WebServiceClient($url);
 
-//$_POST['username'],  //will change per web service(add bookmark,etc)
-$data = array("username" => "lowryp", 
-				"password" => "h9zHEcBww");//$_POST['password']);
+
+//Checking both Username and Password being set
+$_SESSION['errors'] = array();
+$required = array("username" => "Please fill in the username",
+                  "password" => "Please fill in the password",
+                 );
+foreach ($required as $field => $err) {
+  if (!isset($_POST[$field]) || empty($_POST[$field])) {
+    $_SESSION['errors'][$field] = $err;
+  }
+}
+
+
+//Submitting Username and Password to JSON WebService
+$data = array("username" => $_POST['username'], 
+				"password" => $_POST['password']);//$_POST['password']);
 				
 $action = "authenticate";  //will change per web service (add bookmark, etc)
 
@@ -35,11 +47,13 @@ $result = $client->send();
 //debugging output (remove later)
 //var_dump($client);
 //var_dump($result);
+
+//full Decode
 $jsonResult = json_decode($result);  
 
 
 
-
+//Checking for JSON Errors
 if (json_last_error() !== JSON_ERROR_NONE) {
 
   //may want to redirect if JSON is wrong
@@ -48,7 +62,7 @@ if (json_last_error() !== JSON_ERROR_NONE) {
   exit;
 }
 
-
+//Checking JSON Object Variables
 if ($jsonResult->result == "Success") {
   print("<div>It Printed Success</div>");
 } else {
@@ -56,6 +70,13 @@ if ($jsonResult->result == "Success") {
 }
 
 
-
+//random dump
 print("<br><br>");
 var_dump($jsonResult);  //can use web service stuff here
+print("<br>" . $_POST['username'] . "  " . $_POST['password']);
+
+
+//End of Isset and Empty error checks, sends user back to Login
+if (count($_SESSION['errors']) > 0) {
+	die(header("Location: login.php"));
+}
