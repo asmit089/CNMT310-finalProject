74f3e6d72c10967616e97d72d585fc6b25830512
ajohn723 <127328907+ajohn723@users.noreply.github.com>
2025-04-16 11:10:32 -0500

    Delete final-project/creds.php

--- final-project/creds.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//old business
-define("PASSWORD_HASH",'$2y$10$R.FMq5/4D8mEASVaiEW6DOVz6gyW4XyUfvQPHZt84uWx5dcoQr236');  //all uppercase constants are easy
-//abc123
-//
-define("APIKEY","api67");
-define("APIHASH","brtfedd");
-
